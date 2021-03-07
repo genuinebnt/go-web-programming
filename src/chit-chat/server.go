8a90932ae156d6c08f0954bded56f3917b32ea39
genuinebnt <genuinebnt@gmail.com>
2021-03-07 10:48:04 +0530

    created basic structure for a chitchat server

--- src/chit-chat/server.go
+++ src/chit-chat/server.go
@@ -4,7 +4,7 @@ import (
 	"html/template"
 	"net/http"
 )
-
+/*
 func index(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"templates/layout.html",
@@ -17,25 +17,25 @@ func index(w http.ResponseWriter, r *http.Request) {
 		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }
-
+*/
 func main() {
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir("/public"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
-	mux.HandleFunc("/", index)
-	mux.HandleFunc("/err", err)
+	//mux.HandleFunc("/", index)
+	// mux.HandleFunc("/err", err)
 
-	mux.HandleFunc("/login", login)
-	mux.HandleFunc("/logout", logout)
-	mux.HandleFunc("/signup", signup)
-	mux.HandleFunc("/signup_account", signupAccount)
-	mux.HandleFunc("/authenticate", authenticate)
+	// mux.HandleFunc("/login", login)
+	// mux.HandleFunc("/logout", logout)
+	// mux.HandleFunc("/signup", signup)
+	// mux.HandleFunc("/signup_account", signupAccount)
+	// mux.HandleFunc("/authenticate", authenticate)
 
-	mux.HandleFunc("/thread/new", newThread)
-	mux.HandleFunc("/thread/create", createThread)
-	mux.HandleFunc("/thread/post", postThread)
-	mux.HandleFunc("/thread/read", readThread)
+	// mux.HandleFunc("/thread/new", newThread)
+	// mux.HandleFunc("/thread/create", createThread)
+	// mux.HandleFunc("/thread/post", postThread)
+	// mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
